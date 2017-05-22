a0d3d6cf2395e8873a4cddc6a07b5d9f4060b0ef
tonghao <tonghao@30.com>
2017-05-22 23:11:54 +0800

    ...

--- src/test/java/InjecTest.java
+++ src/test/java/InjecTest.java
@@ -1,4 +1,6 @@
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,4 +12,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 "classpath:servlet-dispatcher.xml"})
 public class InjecTest {
 
+    @Value("${ly.key}")
+    private String key;
+
+    @Test
+    public void test(){
+        System.out.println("*********"+key);
+    }
 }
