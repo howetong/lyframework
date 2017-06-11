51f4643a71a95034df71cca6e3432e04df704de5
tonghao <tonghao@30.com>
2017-06-11 19:43:41 +0800

    ly框架功能开发，新增首页菜单项

--- src/main/java/cn/core/security/controller/SysMainController.java
+++ src/main/java/cn/core/security/controller/SysMainController.java
@@ -1,11 +1,18 @@
 package cn.core.security.controller;
 
 import cn.core.controller.BaseController;
+import cn.core.domain.BaseBean;
+import cn.core.domain.ParentChildrenBean;
+import cn.core.security.domain.SysMenu;
+import cn.core.security.service.ISysMenuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,13 +23,18 @@ import java.util.Map;
 @RequestMapping("/admin")
 public class SysMainController extends BaseController {
 
+    @Resource
+    protected ISysMenuService service;
+
     @RequestMapping(method= RequestMethod.GET)
-    public ModelAndView doIndex() throws Exception{
+    private ModelAndView doIndex() throws Exception{
         ModelAndView mv = new ModelAndView();
+        Map<String, String> sortedCondition = new HashMap<String, String>();
+        sortedCondition.put("sequence", "ASC");
         //查询所有状态正常的一级菜单
-//        List<SysMenu> rootMenu = this.service.queryByProperties(new String[]{ParentChildrenBean.PARENT_FIELD,BaseBean.STATUS_FIELD},
-//                new Object[]{null,false},sortedCondition,SysMenu.class);
-//        mv.addObject("menus",rootMenu);
+        List<SysMenu> rootMenu = this.service.queryByProperties(new String[]{ParentChildrenBean.PARENT_FIELD, BaseBean.STATUS_FIELD},
+                new Object[]{null,false},sortedCondition,SysMenu.class);
+        mv.addObject("menus",rootMenu);
         mv.setViewName(getTemplateDirectoryView()+"/index");
         return mv;
     }
